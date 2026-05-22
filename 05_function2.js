bb18f650fded941d13af52417fb842864728fd86
sangmin <tjfl9831@gmail.com>
2026-05-22 12:11:01 +0900

    feat: 익명함수, 콜백함수 및 호이스팅 예제 추가

---
+++ 05_function2.js
@@ -0,0 +1,23 @@
+(function () {
+  // 익명함수
+  console.log("나는 존재한다");
+})();
+// 한번쓰고 버릴떄
+
+const f = function (fn) {
+  fn();
+  console.log("나는 존재한다 2");
+};
+
+f(function () {
+  console.log("ㅎㅇ");
+});
+
+console.log("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+
+a(); // 선언 전
+//html script -> 언제 해석될지 모름. html에 button, onclick 속성에 js 함수. html이 먼저 등장해서 위치를 잡은다음 뒤에 script, (var, function) /hoist (감아올리기) -> let, const, function? -> let, const, 표현식 = 익명함수.
+
+function a() {
+  console.log("호이스팅");
+}
