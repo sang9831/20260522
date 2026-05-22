1a6b30c516cfafa02c17b8dea475edb0220429bc
sangmin <tjfl9831@gmail.com>
2026-05-22 10:02:52 +0900

    feat: 역방향 및 이중 for 루프 예제 추가

--- 01_for.js
+++ 01_for.js
@@ -5,3 +5,18 @@ console.log(arr[0], arr[1], arr[2], arr[3], arr[4], arr[5], arr[6]);
 for (let i = 0; i < arr.length; i++) {
   console.log(arr[i]);
 }
+console.log("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+
+for (let i = arr.length - 1; i >= 0; i--) {
+  console.log(arr[i]);
+}
+
+//for-in, for-of는 역방향 안됨
+console.log("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+
+for (let i = 0; i < 3; i++) {
+  for (let j = 0; j < 3; j++) {
+    console.log(`i=${i},j=${j}`);
+  }
+}
+console.log("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
