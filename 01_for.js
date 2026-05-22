314967af5a953e25b4f9c29d91e981d08918ee9d
sangmin <tjfl9831@gmail.com>
2026-05-22 09:33:35 +0900

    feat: for 루프를 사용한 배열 순회 코드 작성

---
+++ 01_for.js
@@ -0,0 +1,7 @@
+const arr = ["아침", "간식", "점심", "간식", "저녁", "간식", "야식"];
+
+console.log(arr[0], arr[1], arr[2], arr[3], arr[4], arr[5], arr[6]);
+
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
